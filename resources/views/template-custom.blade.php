8a8a741ab8e5b8f0aa7c2fb29698dc5d50b0f27f
tbono <tbono@w2ogroup.com>
2019-02-16 10:05:17 -0500

    added the story and updarted the custom template and css tweek for less padding on mobile

--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -4,9 +4,15 @@
 
 @extends('layouts.app')
 
-@section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
-@endsection
+<div class="container custom">
+	<div class="row">
+		<div class="col-12">
+			@section('content')
+			  @while(have_posts()) @php the_post() @endphp
+			    @include('partials.page-header')
+			    @include('partials.content-page')
+			  @endwhile
+			@endsection
+		</div>
+	</div>
+</div>
