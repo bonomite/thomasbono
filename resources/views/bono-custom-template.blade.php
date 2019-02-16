1a1d939ae49c4709be47dbb2d25fd02ddcbddb55
tbono <tbono@w2ogroup.com>
2019-02-16 10:12:35 -0500

    custom templte

---
+++ resources/views/bono-custom-template.blade.php
@@ -0,0 +1,18 @@
+{{--
+  Template Name: Bono Custom Template
+--}}
+
+@extends('layouts.app')
+
+<div class="container custom">
+	<div class="row">
+		<div class="col-12">
+			@section('content')
+			  @while(have_posts()) @php the_post() @endphp
+			    @include('partials.page-header')
+			    @include('partials.content-page')
+			  @endwhile
+			@endsection
+		</div>
+	</div>
+</div>
